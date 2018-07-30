ba4997d5d419358a8ded6dc94343de4a1594f15a
Татаринова Татьяна Викторовна <ttatarinova@technoserv.ts>
2018-07-30 15:31:17 +0300

    Перенос теста на виртуальную машину (RemoteWebDriver)

--- src/main/java/pages/yandex/YandexMarketPage.java
+++ src/main/java/pages/yandex/YandexMarketPage.java
@@ -6,16 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.*;
 import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.Link;
 import ru.yandex.qatools.htmlelements.element.TextInput;
 import ru.yandex.qatools.htmlelements.loader.HtmlElementLoader;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
 
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -40,8 +38,8 @@ public class YandexMarketPage extends AbstractYandexPage {
 
         priceFrom.sendKeys(valueFrom);
         priceTo.sendKeys(valueTo);
-        //Thread.sleep(5000);
-        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.xpath(""))));
+        //wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.xpath("//*[contains(@class,'spin2_progress_yes')]"))));   // не работает:(
+        wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.xpath("//*[contains(@class,'preloadable__preloader_visibility_visible')]"))));  // не работает:(
 
         List<WebElement> searchResults = driver.findElements(By.xpath("//div[contains(@data-id, 'model')]//a//*[@class='price']"));
 
